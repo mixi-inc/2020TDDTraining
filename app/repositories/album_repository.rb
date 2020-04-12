6056ddc27c5b2458d4edc46c0cf0ea163b83cff3
rockname <8536870+rockname@users.noreply.github.com>
2020-04-13 01:17:20 +0900

    Implement repositories

--- app/repositories/album_repository.rb
+++ app/repositories/album_repository.rb
@@ -4,6 +4,16 @@ class AlbumRepository
   end
 
   def find_by(id:)
-
+    album = @album.find_by(id: id)
+    AlbumEntity.new(
+      id: album.id,
+      children: album.children.map do |child|
+        ChildEntity.new(
+          id: child.id,
+          name: child.name,
+          birthday: child.birthday
+        )
+      end
+    )
   end
 end
--- app/repositories/photobook_repository.rb
+++ app/repositories/photobook_repository.rb
@@ -13,7 +13,39 @@ class PhotobookRepository
     subtitle:,
     photobook_pages:
   )
-
+    photobook = @photobook.create(
+      album_id: album_id,
+      account_id: account_id,
+      cover_media_id: cover_media_id,
+      cover_media_taken_at: cover_media_taken_at,
+      title: title,
+      subtitle: subtitle
+    )
+    pages = photobook_pages.map do |new_page|
+      PhotobookPage.create(
+        photobook_id: photobook.id,
+        page_number: new_page[:page_number],
+        media_id: new_page[:media_id],
+        comment: new_page[:comment]
+      )
+    end
+    PhotobookEntity.new(
+      id: photobook.id,
+      album_id: photobook.album_id,
+      account_id: photobook.account_id,
+      cover_media_id: photobook.cover_media_id,
+      cover_media_taken_at: photobook.cover_media_taken_at,
+      title: photobook.title,
+      subtitle: photobook.subtitle,
+      photobook_pages: pages.map do |page|
+        PhotobookPageEntity.new(
+          id: page.id,
+          photobook_id: page.photobook_id,
+          page_number: page.page_number,
+          media_id: page.media_id,
+          comment: page.comment
+        )
+      end
+    )
   end
 end
-
