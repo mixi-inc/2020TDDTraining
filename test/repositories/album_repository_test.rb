73d11fd7a4ad801a6ce56491f51ad7d610f0f2e9
rockname <8536870+rockname@users.noreply.github.com>
2020-04-13 01:14:31 +0900

    Add repository tests

--- test/repositories/album_repository_test.rb
+++ test/repositories/album_repository_test.rb
@@ -1,5 +1,37 @@
 require 'test_helper'
 
 class AlbumRepositoryTest < ActiveSupport::TestCase
+  test 'should return AlbumEntity' do
+    repository = AlbumRepository.new(
+      album_model: MockAlbum.new(children: [
+        MockChild.new(name: '太郎', birthday: Time.zone.parse('2019/01/01')),
+        MockChild.new(name: '次郎', birthday: Time.zone.parse('2020/01/01'))
+      ])
+    )
+    album = repository.find_by(id: 0)
+    assert(album.is_a?(AlbumEntity))
+    assert_equal(2, album.children.count)
+  end
+end
 
+class MockAlbum
+  attr_accessor :id, :children
+
+  def initialize(id: nil, children:)
+    @children = children
+  end
+
+  def find_by(id:)
+    MockAlbum.new(id: id, children: @children)
+  end
+end
+
+class MockChild
+  attr_accessor :id, :name, :birthday
+
+  def initialize(id: nil, name:, birthday:)
+    @id = id
+    @name = name
+    @birthday = birthday
+  end
 end
--- test/repositories/photobook_repository_test.rb
+++ test/repositories/photobook_repository_test.rb
@@ -1,5 +1,96 @@
 require 'test_helper'
 
 class PhotobookRepositoryTest < ActiveSupport::TestCase
+  test 'should return PhotobookEntity' do
+    repository = PhotobookRepository.new(
+      photobook_model: MockPhotobook.new,
+      photobook_page_model: MockPhotobookPage.new
+    )
+    photobook = repository.create(
+      album_id: 0,
+      account_id: 0,
+      cover_media_id: 0,
+      cover_media_taken_at: Time.zone.parse('2020/04/01'),
+      title: '2020年4月のアルバム',
+      subtitle: '太郎1歳3ヶ月・次郎0歳3ヶ月',
+      photobook_pages: [
+        {
+          page_number: 1,
+          media_id: 1,
+          comment: 'いいね'
+        }, {
+          page_number: 2,
+          media_id: 2,
+          comment: 'いいよ'
+        }
+      ]
+    )
+    assert(photobook.is_a?(PhotobookEntity))
+    assert_equal(0, photobook.album_id)
+    assert_equal(0, photobook.account_id)
+    assert_equal(0, photobook.cover_media_id)
+    assert_equal(Time.zone.parse('2020/04/01'), photobook.cover_media_taken_at)
+    assert_equal('2020年4月のアルバム', photobook.title)
+    assert_equal('太郎1歳3ヶ月・次郎0歳3ヶ月', photobook.subtitle)
+    assert_equal(2, photobook.photobook_pages.count)
+  end
+end
 
+class MockPhotobook
+  attr_accessor :id, :album_id, :account_id, :cover_media_id, :cover_media_taken_at, :title, :subtitle
+
+  def initialize(
+    id: nil,
+    album_id: nil,
+    account_id: nil,
+    cover_media_id: nil,
+    cover_media_taken_at: nil,
+    title: nil,
+    subtitle: nil
+  )
+    @id = id
+    @album_id = album_id
+    @account_id = account_id
+    @cover_media_id = cover_media_id
+    @cover_media_taken_at = cover_media_taken_at
+    @title = title
+    @subtitle = subtitle
+  end
+
+  def create(
+    album_id:,
+    account_id:,
+    cover_media_id:,
+    cover_media_taken_at:,
+    title:,
+    subtitle:
+  )
+    MockPhotobook.new(
+      id: 0,
+      album_id: album_id,
+      account_id: account_id,
+      cover_media_id: cover_media_id,
+      cover_media_taken_at: cover_media_taken_at,
+      title: title,
+      subtitle: subtitle
+    )
+  end
+end
+
+class MockPhotobookPage
+  attr_accessor :create_called_count
+
+  def initialize
+    @create_called_count = 0
+    @import_called = false
+  end
+
+  def create(
+    photobook_id:,
+    page_number:,
+    media_id:,
+    comment:
+  )
+    @create_called_count += 1
+  end
 end
