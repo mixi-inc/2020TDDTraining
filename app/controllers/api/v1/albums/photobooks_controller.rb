ea640ad437eb988f26eada05d93e4ba8449166a0
rockname <8536870+rockname@users.noreply.github.com>
2020-04-12 18:06:49 +0900

    Extract logic into service

--- app/controllers/api/v1/albums/photobooks_controller.rb
+++ app/controllers/api/v1/albums/photobooks_controller.rb
@@ -3,86 +3,43 @@ module Api
     module Albums
       class PhotobooksController < ApplicationController
         def create
-          album = Album.find_by(id: params[:album_id])
-          if album.nil?
+          begin
+            photobook = CreatePhotobookService.new.call(
+              album_id: params[:album_id],
+              account_id: params[:account_id],
+              title: params[:title],
+              subtitle: params[:subtitle],
+              cover_media_id: params[:cover_media_id],
+              cover_media_taken_at: params[:cover_media_taken_at],
+              photobook_pages: params[:photobook_pages]
+            )
+          rescue CreatePhotobookService::AlbumNotFoundError
             return render json: {
               code: 'album_not_found',
               message: 'アルバムが存在していません'
             }, status: 404
-          end
-
-          if params[:title].present? && params[:title].length > 20
+          rescue CreatePhotobookService::TitleTooLongError
             return render json: {
               code: 'too_long_photobook_title',
               message: 'フォトブックのタイトルが文字数上限を超えています'
             }, status: 400
-          end
-
-          if params[:subtitle].present? && params[:subtitle].length > 20
+          rescue CreatePhotobookService::SubtitleTooLongError
             return render json: {
               code: 'too_long_photobook_subtitle',
               message: 'フォトブックのサブタイトルが文字数上限を超えています'
             }, status: 400
-          end
-
-          if params[:photobook_pages].any? { |page| (page[:comment] || '').length > 200 }
+          rescue CreatePhotobookService::CommentTooLongError
             return render json: {
               code: 'too_long_photobook_comment',
               message: 'フォトブックのコメントが文字数上限を超えています'
             }, status: 400
           end
 
-          taken_date = Time.zone.parse(params[:cover_media_taken_at])
-          title = params[:title] || "#{taken_date.year}年#{taken_date.month}月のアルバム"
-
-          subtitle = params[:subtitle] || album.children.map do |extract_child|
-            age = lunar_age(extract_child, at: taken_date)
-            extract_child.name + (age || '')
-          end.join('・')
-          subtitle = subtitle.length > 20 ? 'Family Album' : subtitle
-
-          photobook = ActiveRecord::Base.transaction do
-            Photobook.create(
-              album_id: params[:album_id],
-              account_id: params[:account_id],
-              cover_media_id: params[:cover_media_id],
-              cover_media_taken_at: params[:cover_media_taken_at],
-              title: title,
-              subtitle: subtitle
-            )
-          end
-
-          pages = params[:photobook_pages].map do |new_page|
-            PhotobookPage.new(
-              photobook_id: photobook.id,
-              page_number: new_page[:page_number],
-              media_id: new_page[:media_id],
-              comment: new_page[:comment]
-            )
-          end
-          PhotobookPage.import(pages)
-
           render json: {
             id: photobook.id,
             title: photobook.title,
             subtitle: photobook.subtitle
           }, status: 201
-        end
-
-        def lunar_age(child, at: Time.zone.now)
-          return '' if child.birthday > at.to_date
-
-          years, months = calc_year_month(child, at)
-          "#{years}歳#{months}ヶ月"
-        end
-
-        def calc_year_month(child, at) 
-          return if child.birthday.blank?
-
-          years = (at.strftime('%Y%m').to_i - child.birthday.strftime('%Y%m').to_i) / 100
-          months = at.month.to_i - child.birthday.month.to_i
-          months += 12 if months.negative?
-          [years, months]
         end
       end
     end
---
+++ app/services/create_photobook_service.rb
@@ -0,0 +1,77 @@
+class CreatePhotobookService
+  class AlbumNotFoundError < StandardError; end
+  class TitleTooLongError < StandardError; end
+  class SubtitleTooLongError < StandardError; end
+  class CommentTooLongError < StandardError; end
+
+  TITLE_MAX_LENGTH = 20
+  SUBTITLE_MAX_LENGTH = 20
+  COMMENT_MAX_LENGTH = 200
+
+  def call(
+    album_id:,
+    account_id:,
+    title:,
+    subtitle:, 
+    cover_media_id:,
+    cover_media_taken_at:,
+    photobook_pages:
+  )
+    album = Album.find_by(id: album_id)
+    raise AlbumNotFoundError if album.nil?
+
+    raise TitleTooLongError if title.present? && title.length > TITLE_MAX_LENGTH
+
+    raise SubtitleTooLongError if subtitle.present? && subtitle.length > SUBTITLE_MAX_LENGTH
+
+    raise CommentTooLongError if photobook_pages.any? { |page| (page[:comment] || '').length > COMMENT_MAX_LENGTH }
+
+    taken_date = Time.zone.parse(cover_media_taken_at)
+    title ||= "#{taken_date.year}年#{taken_date.month}月のアルバム"
+
+    subtitle ||= album.children.map do |extract_child|
+      age = lunar_age(extract_child, at: taken_date)
+      extract_child.name + (age || '')
+    end.join('・')
+    subtitle = subtitle.length > SUBTITLE_MAX_LENGTH ? 'Family Album' : subtitle
+
+    photobook = ActiveRecord::Base.transaction do
+      Photobook.create(
+        album_id: album_id,
+        account_id: account_id,
+        cover_media_id: cover_media_id,
+        cover_media_taken_at: cover_media_taken_at,
+        title: title,
+        subtitle: subtitle
+      )
+    end
+
+    pages = photobook_pages.map do |new_page|
+      PhotobookPage.new(
+        photobook_id: photobook.id,
+        page_number: new_page[:page_number],
+        media_id: new_page[:media_id],
+        comment: new_page[:comment]
+      )
+    end
+    PhotobookPage.import(pages)
+
+    photobook
+  end
+
+  def lunar_age(child, at: Time.zone.now)
+    return '' if child.birthday > at.to_date
+
+    years, months = calc_year_month(child, at)
+    "#{years}歳#{months}ヶ月"
+  end
+
+  def calc_year_month(child, at) 
+    return if child.birthday.blank?
+
+    years = (at.strftime('%Y%m').to_i - child.birthday.strftime('%Y%m').to_i) / 100
+    months = at.month.to_i - child.birthday.month.to_i
+    months += 12 if months.negative?
+    [years, months]
+  end
+end
