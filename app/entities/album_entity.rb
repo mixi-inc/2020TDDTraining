092ecbf019cd69f4034ff902261a81809832536d
rockname <8536870+rockname@users.noreply.github.com>
2020-04-13 23:51:53 +0900

    Fix entities

--- app/entities/album_entity.rb
+++ app/entities/album_entity.rb
@@ -1,4 +1,6 @@
 class AlbumEntity
+  attr_accessor :id, :children
+
   def initialize(id:, children:)
     @id = id
     @children = children
--- app/entities/child_entity.rb
+++ app/entities/child_entity.rb
@@ -1,7 +1,7 @@
 class ChildEntity
   attr_accessor :name, :birthday
 
-  def initialize(id:, name:, :birthday)
+  def initialize(id:, name:, birthday:)
     @id = id
     @name = name
     @birthday = birthday
