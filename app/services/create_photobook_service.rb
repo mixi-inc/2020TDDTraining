eae0d9ccd0b294b053422dc68ee534c9676cc3d0
rockname <8536870+rockname@users.noreply.github.com>
2020-04-13 22:12:56 +0900

    Add empty CreatePhotobookService

---
+++ app/services/create_photobook_service.rb
@@ -0,0 +1,13 @@
+class CreatePhotobookService
+  def call(
+    album_id:,
+    account_id:,
+    title:,
+    subtitle:, 
+    cover_media_id:,
+    cover_media_taken_at:,
+    photobook_pages:
+  )
+
+  end
+end
